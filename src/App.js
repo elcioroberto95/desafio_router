e2bd5676f755a1e3684a8df34f28825352e7a03d
elcioroberto95 <elcioroberto1995@gmail.com>
2021-01-13 19:49:45 -0300

    rotas implementadas

--- src/App.js
+++ src/App.js
@@ -2,6 +2,25 @@ import react from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 const App = () => {
-  return <></>;
+  return (
+    <>
+      <BrowserRouter>
+        <Head />
+        <Header />
+        <Switch>
+          <Route exact path="/">
+            <Produtos />
+          </Route>
+          <Route path="/contato">
+            <Contato />
+          </Route>
+          <Route path="/produto/:id">
+            <Produto />
+          </Route>
+        </Switch>
+        <Footer />
+      </BrowserRouter>
+    </>
+  );
 };
 export default App;
